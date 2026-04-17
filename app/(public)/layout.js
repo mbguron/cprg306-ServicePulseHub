0e205ac00d294b60c3e47184d4b6e436d81a0034
wendy nguyen <uyennhp134@gmail.com>
2026-04-17 00:23:36 -0600

    added Gemini Chatbot

--- app/(public)/layout.js
+++ app/(public)/layout.js
@@ -1,11 +1,13 @@
 import NavBar from "@/components/layouts/NavBar";
 import Footer from "@/components/layouts/Footer";
+import Chatbot from "@/components/Chatbot";
 
 export default function PublicLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar />
       <main className="flex-grow">{children}</main>
+      <Chatbot />
       <Footer />
     </div>
   );
